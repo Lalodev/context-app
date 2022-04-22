eec46bc6a04fb753c7c80dfdecb915668837acaa
Lalodev <laloing87@gmail.com>
2022-04-22 11:18:15 -0600

    done

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,38 @@
+import React, { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+const initialUser = {
+  id: 1,
+  name: "Lalo",
+  favoriteMovies: [1, 2],
+};
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(initialUser);
+
+  const login = () => {
+    setUser(initialUser);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  const toggleFavoriteMovieToUser = (movieId) => {
+    //alert(movieId);
+    const isFavorite = user.favoriteMovies.includes(movieId);
+    const favoriteMovies = isFavorite
+      ? user.favoriteMovies.filter((favMovId) => favMovId !== movieId)
+      : [...user.favoriteMovies, movieId];
+
+    setUser({ ...user, favoriteMovies });
+  };
+
+  const data = { user, login, logout, toggleFavoriteMovieToUser };
+
+  return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
+};
+
+export { UserProvider };
+export default UserContext;
